f3af44322abcf652ea6f3f697c33824d9334fd3d
Aleix Balletbó <aleixballetbo@gmail.com>
2019-02-20 17:36:19 +0100

    Add new train service and update forecast library

--- src/main/java/com/qrapids/forecast_rest/services/ForecastService.java
+++ src/main/java/com/qrapids/forecast_rest/services/ForecastService.java
@@ -1,12 +1,18 @@
 package com.qrapids.forecast_rest.services;
 
 import Forecast.Common;
+import Forecast.Elastic_RForecast;
 import Forecast.ForecastDTO;
 import com.qrapids.forecast_rest.configuration.Connection;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,23 +23,39 @@ public class ForecastService {
     @Autowired
     private Connection connection;
 
+    @RequestMapping("api/ForecastTechniques")
+    public Common.ForecastTechnique[] getForecastTechniques () {
+        return Elastic_RForecast.getForecastTechniques();
+    }
+
+    @RequestMapping("api/Train")
+    public void train(@RequestParam("host") String host, @RequestParam("port") String port, @RequestParam("path") String path, @RequestParam("user") String user, @RequestParam("pwd") String pwd, @RequestParam("index") String index, @RequestParam("elements") List<String> elements, @RequestParam("frequency") String freq) {
+        Elastic_RForecast elastic_rForecast = connection.getConnection(host, port, path, user, pwd);
+        for (Common.ForecastTechnique forecastTechnique : Common.ForecastTechnique.values()) {
+            try {
+                String[] result = elastic_rForecast.multipleElementTrain(elements.toArray(new String[0]), index, freq, forecastTechnique);
+            }
+            catch (Exception e) {}
+        }
+    }
+
     @RequestMapping("/api/Metrics/Forecast")
-    public List<ForecastDTO> getForecastMetric(@RequestParam("host") String host, @RequestParam("port") String port, @RequestParam("path") String path, @RequestParam("user") String user, @RequestParam("pwd") String pwd, @RequestParam("index_metrics") String indexMetrics, @RequestParam("metric") List<String> metric, @RequestParam("frequency") String freq, @RequestParam("horizon") String horizon) {
-        if (metric.size() < 2)
-            return new ArrayList<ForecastDTO>() {{
-                add(connection.getConnection(host, port, path, user, pwd).metricForecast(indexMetrics, metric.get(0), freq, horizon, Common.ForecastTechnique.ETS));
-            }};
-        else
-            return connection.getConnection(host, port, path, user, pwd).multipleMetricsForecast(indexMetrics, metric.toArray(new String[0]), freq, horizon, Common.ForecastTechnique.ETS);
+    public List<ForecastDTO> getForecastMetric(@RequestParam("host") String host, @RequestParam("port") String port, @RequestParam("path") String path, @RequestParam("user") String user, @RequestParam("pwd") String pwd, @RequestParam("index_metrics") String indexMetrics, @RequestParam("metric") List<String> metric, @RequestParam("frequency") String freq, @RequestParam("horizon") String horizon, @RequestParam("technique") String technique) throws Exception {
+        return getForecast(host, port, path, user, pwd, indexMetrics, metric, freq, horizon, technique);
     }
 
     @RequestMapping("/api/QualityFactors/Forecast")
-    public List<ForecastDTO> getForecastFactor(@RequestParam("host") String host, @RequestParam("port") String port, @RequestParam("path") String path, @RequestParam("user") String user, @RequestParam("pwd") String pwd, @RequestParam("index_factors") String indexFactors, @RequestParam("factor") List<String> factor, @RequestParam("frequency") String freq, @RequestParam("horizon") String horizon) {
-        if (factor.size() < 2)
-            return new ArrayList<ForecastDTO>() {{
-                add(connection.getConnection(host, port, path, user, pwd).factorForecast(indexFactors, factor.get(0), freq, horizon, Common.ForecastTechnique.ETS));
-            }};
-        else
-            return connection.getConnection(host, port, path, user, pwd).multipleFactorsForecast(indexFactors, factor.toArray(new String[0]), freq, horizon, Common.ForecastTechnique.ETS);
+    public List<ForecastDTO> getForecastFactor(@RequestParam("host") String host, @RequestParam("port") String port, @RequestParam("path") String path, @RequestParam("user") String user, @RequestParam("pwd") String pwd, @RequestParam("index_factors") String indexFactors, @RequestParam("factor") List<String> factor, @RequestParam("frequency") String freq, @RequestParam("horizon") String horizon, @RequestParam("technique") String technique) throws  Exception {
+        return getForecast(host, port, path, user, pwd, indexFactors, factor, freq, horizon, technique);
+    }
+
+    private List<ForecastDTO> getForecast(String host, String port, String path, String user, String pwd, String indexFactors, List<String> factor, String freq, String horizon, String technique) throws REXPMismatchException, REngineException {
+        try {
+            Common.ForecastTechnique forecastTechnique = Common.ForecastTechnique.valueOf(technique);
+            return connection.getConnection(host, port, path, user, pwd).multipleElementForecast(factor.toArray(new String[0]), indexFactors, freq, horizon, forecastTechnique);
+        }
+        catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Forecast technique not supported");
+        }
     }
 }
--- src/test/java/com/qrapids/forecast_rest/services/ForecastServiceTest.java
+++ src/test/java/com/qrapids/forecast_rest/services/ForecastServiceTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
@@ -55,6 +57,87 @@ public class ForecastServiceTest {
     }
 
     @Test
+    public void getForecastTechniques() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/ForecastTechniques");
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(10)))
+                .andExpect(jsonPath("$[0]", is(Common.ForecastTechnique.ARIMA.toString())))
+                .andExpect(jsonPath("$[1]", is(Common.ForecastTechnique.ARIMA_FORCE_SEASONALITY.toString())))
+                .andExpect(jsonPath("$[2]", is(Common.ForecastTechnique.THETA.toString())))
+                .andExpect(jsonPath("$[3]", is(Common.ForecastTechnique.ETS.toString())))
+                .andExpect(jsonPath("$[4]", is(Common.ForecastTechnique.ETSDAMPED.toString())))
+                .andExpect(jsonPath("$[5]", is(Common.ForecastTechnique.BAGGEDETS.toString())))
+                .andExpect(jsonPath("$[6]", is(Common.ForecastTechnique.STL.toString())))
+                .andExpect(jsonPath("$[7]", is(Common.ForecastTechnique.NN.toString())))
+                .andExpect(jsonPath("$[8]", is(Common.ForecastTechnique.HYBRID.toString())))
+                .andExpect(jsonPath("$[9]", is(Common.ForecastTechnique.PROPHET.toString())))
+                .andDo(document("ForecastTechniques",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("[]")
+                                        .description("Forecast techiques"))));
+    }
+
+    @Test
+    public void train() throws Exception {
+        String host = "host";
+        String port = "8080";
+        String path = "path";
+        String user = "user";
+        String pwd = "pwd";
+        String index = "indexMetrics";
+        String[] elements = {"testperformance"};
+        String frequency = "7";
+
+        Elastic_RForecast elastic_rForecast = mock(Elastic_RForecast.class);
+        when(connection.getConnection(host,port,path,user,pwd)).thenReturn(elastic_rForecast);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/Train")
+                .param("host", host)
+                .param("port", port)
+                .param("path", path)
+                .param("user", user)
+                .param("pwd", pwd)
+                .param("index", index)
+                .param("elements", elements)
+                .param("frequency", frequency);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andDo(document("Train",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("host")
+                                        .description("ElasticSearch host"),
+                                parameterWithName("port")
+                                        .description("ElasticSearch port"),
+                                parameterWithName("path")
+                                        .description("ElasticSearch path"),
+                                parameterWithName("user")
+                                        .description("ElasticSearch user"),
+                                parameterWithName("pwd")
+                                        .description("ElasticSearch password"),
+                                parameterWithName("index")
+                                        .description("ElasticSearch name of metrics or factors index"),
+                                parameterWithName("elements")
+                                        .description("List of elements to forecast"),
+                                parameterWithName("frequency")
+                                        .description("Amount of days conforming the natural time period of the data samples")
+                        )));
+
+        verify(elastic_rForecast, times(Common.ForecastTechnique.values().length)).multipleElementTrain(eq(elements),eq(index),eq(frequency), any(Common.ForecastTechnique.class));
+        verifyNoMoreInteractions(elastic_rForecast);
+        verify(connection, times(1)).getConnection(host,port,path,user,pwd);
+        verifyNoMoreInteractions(connection);
+    }
+
+    @Test
     public void getForecastMetric() throws Exception {
         String host = "host";
         String port = "8080";
@@ -65,8 +148,10 @@ public class ForecastServiceTest {
         String[] metric = {"testperformance"};
         String frequency = "7";
         String horizon = "10";
+        String technique = "ETS";
 
-        ForecastDTO forecastDTO = new ForecastDTO("testperformance");
+        ForecastDTO forecastDTO = new ForecastDTO();
+        forecastDTO.setId("testperformance");
         double[] lower80 = {0.6414524692520523,0.6310027287500249,0.6229788194502033,0.6162098305036225,0.6102422881085776,0.6048436667711347,0.5998758631263202};
         forecastDTO.setLower80(lower80);
         double[] lower95 = {0.6281052189200053,0.612123719944535,0.5998522096396154,0.5894999343947172,0.5803733658781132,0.5721168870347814,0.564519286994423};
@@ -77,10 +162,12 @@ public class ForecastServiceTest {
         forecastDTO.setUpper80(upper80);
         double[] upper95 = {0.705226831705894,0.7212083306813643,0.7334798409862838,0.7438321162311821,0.752958684747786,0.7612151635911178,0.7688127636314762};
         forecastDTO.setUpper95(upper95);
+        ArrayList<ForecastDTO> forecastDTOArrayList = new ArrayList<>();
+        forecastDTOArrayList.add(forecastDTO);
 
         Elastic_RForecast elastic_rForecast = mock(Elastic_RForecast.class);
         when(connection.getConnection(host,port,path,user,pwd)).thenReturn(elastic_rForecast);
-        when(elastic_rForecast.metricForecast(indexMetrics,metric[0],frequency,horizon, Common.ForecastTechnique.ETS)).thenReturn(forecastDTO);
+        when(elastic_rForecast.multipleElementForecast(metric,indexMetrics,frequency,horizon, Common.ForecastTechnique.ETS)).thenReturn(forecastDTOArrayList);
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/api/Metrics/Forecast")
@@ -92,7 +179,8 @@ public class ForecastServiceTest {
                 .param("index_metrics", indexMetrics)
                 .param("metric", metric)
                 .param("frequency", frequency)
-                .param("horizon", horizon);
+                .param("horizon", horizon)
+                .param("technique", technique);
 
         this.mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
@@ -128,7 +216,9 @@ public class ForecastServiceTest {
                                 parameterWithName("frequency")
                                         .description("Amount of days conforming the natural time period of the data samples"),
                                 parameterWithName("horizon")
-                                        .description("Amount of days that the forecasting will cover")
+                                        .description("Amount of days that the forecasting will cover"),
+                                parameterWithName("technique")
+                                        .description("Forecasting technique")
                         ),
                         responseFields(
                                 fieldWithPath("[].id")
@@ -142,13 +232,46 @@ public class ForecastServiceTest {
                                 fieldWithPath("[].upper80")
                                         .description("Upper 80 confidence predicted interval"),
                                 fieldWithPath("[].upper95")
-                                        .description("Upper 95 confidence predicted interval")
+                                        .description("Upper 95 confidence predicted interval"),
+                                fieldWithPath("[].error")
+                                        .description("Description of the forecasting error")
                         )));
 
-        verify(elastic_rForecast, times(1)).metricForecast(indexMetrics,metric[0],frequency,horizon, Common.ForecastTechnique.ETS);
+        verify(elastic_rForecast, times(1)).multipleElementForecast(metric,indexMetrics,frequency,horizon, Common.ForecastTechnique.ETS);
         verifyNoMoreInteractions(elastic_rForecast);
         verify(connection, times(1)).getConnection(host,port,path,user,pwd);
         verifyNoMoreInteractions(connection);
+    }
+
+    @Test
+    public void getForecastMetricWrongTechnique () throws Exception {
+        String host = "host";
+        String port = "8080";
+        String path = "path";
+        String user = "user";
+        String pwd = "pwd";
+        String indexMetrics = "indexMetrics";
+        String[] metric = {"testperformance"};
+        String frequency = "7";
+        String horizon = "10";
+        String technique = "wrong";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/Metrics/Forecast")
+                .param("host", host)
+                .param("port", port)
+                .param("path", path)
+                .param("user", user)
+                .param("pwd", pwd)
+                .param("index_metrics", indexMetrics)
+                .param("metric", metric)
+                .param("frequency", frequency)
+                .param("horizon", horizon)
+                .param("technique", technique);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(status().reason(is("Forecast technique not supported")));
     }
 
     @Test
@@ -162,8 +285,10 @@ public class ForecastServiceTest {
         String[] factor = {"blockingcode"};
         String frequency = "7";
         String horizon = "10";
+        String technique = "ETS";
 
-        ForecastDTO forecastDTO = new ForecastDTO("blockingcode");
+        ForecastDTO forecastDTO = new ForecastDTO();
+        forecastDTO.setId("blockingcode");
         double[] lower80 = {0.6414524692520523,0.6310027287500249,0.6229788194502033,0.6162098305036225,0.6102422881085776,0.6048436667711347,0.5998758631263202};
         forecastDTO.setLower80(lower80);
         double[] lower95 = {0.6281052189200053,0.612123719944535,0.5998522096396154,0.5894999343947172,0.5803733658781132,0.5721168870347814,0.564519286994423};
@@ -174,10 +299,12 @@ public class ForecastServiceTest {
         forecastDTO.setUpper80(upper80);
         double[] upper95 = {0.705226831705894,0.7212083306813643,0.7334798409862838,0.7438321162311821,0.752958684747786,0.7612151635911178,0.7688127636314762};
         forecastDTO.setUpper95(upper95);
+        ArrayList<ForecastDTO> forecastDTOArrayList = new ArrayList<>();
+        forecastDTOArrayList.add(forecastDTO);
 
         Elastic_RForecast elastic_rForecast = mock(Elastic_RForecast.class);
         when(connection.getConnection(host,port,path,user,pwd)).thenReturn(elastic_rForecast);
-        when(elastic_rForecast.factorForecast(indexFactors,factor[0],frequency,horizon, Common.ForecastTechnique.ETS)).thenReturn(forecastDTO);
+        when(elastic_rForecast.multipleElementForecast(factor,indexFactors,frequency,horizon, Common.ForecastTechnique.ETS)).thenReturn(forecastDTOArrayList);
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/api/QualityFactors/Forecast")
@@ -189,7 +316,8 @@ public class ForecastServiceTest {
                 .param("index_factors", indexFactors)
                 .param("factor", factor)
                 .param("frequency", frequency)
-                .param("horizon", horizon);
+                .param("horizon", horizon)
+                .param("technique", technique);
 
         this.mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
@@ -225,7 +353,9 @@ public class ForecastServiceTest {
                                 parameterWithName("frequency")
                                         .description("Amount of days conforming the natural time period of the data samples"),
                                 parameterWithName("horizon")
-                                        .description("Amount of days that the forecasting will cover")
+                                        .description("Amount of days that the forecasting will cover"),
+                                parameterWithName("technique")
+                                        .description("Forecasting technique")
                         ),
                         responseFields(
                                 fieldWithPath("[].id")
@@ -239,12 +369,45 @@ public class ForecastServiceTest {
                                 fieldWithPath("[].upper80")
                                         .description("Upper 80 confidence predicted interval"),
                                 fieldWithPath("[].upper95")
-                                        .description("Upper 95 confidence predicted interval")
+                                        .description("Upper 95 confidence predicted interval"),
+                                fieldWithPath("[].error")
+                                        .description("Description of the forecasting error")
                         )));
 
-        verify(elastic_rForecast, times(1)).factorForecast(indexFactors,factor[0],frequency,horizon, Common.ForecastTechnique.ETS);
+        verify(elastic_rForecast, times(1)).multipleElementForecast(factor,indexFactors,frequency,horizon, Common.ForecastTechnique.ETS);
         verifyNoMoreInteractions(elastic_rForecast);
         verify(connection, times(1)).getConnection(host,port,path,user,pwd);
         verifyNoMoreInteractions(connection);
+    }
+
+    @Test
+    public void getForecastFactorWrongTechnique () throws Exception {
+        String host = "host";
+        String port = "8080";
+        String path = "path";
+        String user = "user";
+        String pwd = "pwd";
+        String indexFactors = "indexFactors";
+        String[] factor = {"blockingcode"};
+        String frequency = "7";
+        String horizon = "10";
+        String technique = "wrong";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/QualityFactors/Forecast")
+                .param("host", host)
+                .param("port", port)
+                .param("path", path)
+                .param("user", user)
+                .param("pwd", pwd)
+                .param("index_factors", indexFactors)
+                .param("factor", factor)
+                .param("frequency", frequency)
+                .param("horizon", horizon)
+                .param("technique", technique);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(status().reason(is("Forecast technique not supported")));
     }
 }
